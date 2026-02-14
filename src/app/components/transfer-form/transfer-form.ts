8f9244ecd0f1440d0b52d4dd76b643aec4302193
lenapuletic <puleticlena@gmail.com>
2026-02-14 09:10:07 +0100

    feat: make a transfer section

---
+++ src/app/components/transfer-form/transfer-form.ts
@@ -0,0 +1,49 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TransactionService } from '../../services/transaction.service';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-transfer-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, CurrencyPipe],
+  templateUrl: './transfer-form.html',
+  styleUrl: './transfer-form.scss',
+})
+export class TransferForm {
+  private fb = inject(FormBuilder);
+  private transactionService = inject(TransactionService);
+
+  readonly fromAccountDisplay = `Free Checking(4692) - $${this.transactionService.balance()}`;
+
+  isPreview = signal(false);
+
+  transferForm = this.fb.group({
+    toAccount: ['', Validators.required],
+    amount: [
+      null as number | null,
+      [Validators.required, Validators.min(0.01), Validators.max(500)],
+    ],
+  });
+
+  onSubmit() {
+    if (this.transferForm.valid) {
+      this.isPreview.set(true);
+    }
+  }
+
+  onTransfer() {
+    const rawValue = this.transferForm.getRawValue();
+    this.transactionService.addTransaction({
+      amount: rawValue.amount!.toString(),
+      categoryCode: '#1180aa',
+      merchant: rawValue.toAccount!,
+      merchantLogo: '/assets/icons/arrows.png',
+      transactionDate: Date.now(),
+      transactionType: 'Online Transfer',
+    });
+
+    this.transferForm.reset();
+    this.isPreview.set(false);
+  }
+}
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TransactionList } from '../../components/transaction-list/transaction-list';
+import { TransferForm } from '../../components/transfer-form/transfer-form';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TransactionList],
+  imports: [TransactionList, TransferForm],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
